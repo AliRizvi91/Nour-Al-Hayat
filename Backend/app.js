9daccab3ec0bb6eed51e3ec1ef5fe97e33306b47
AliRizvi91 <aliattarrizvi@gmail.com>
2025-01-30 08:53:48 +0500

    Check 4

--- Backend/app.js
+++ Backend/app.js
@@ -43,7 +43,7 @@ app.use(cors());
 
 const corsOptions = {
   origin: process.env.FRONTENDURL, // Replace with your frontend URL
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify any headers if needed
 };
 
